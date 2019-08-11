1f1187641f085380aada44ef9dec4d0a40014e54
Muhammed Erdem <iamuhammederdem@gmail.com>
2019-08-11 13:17:16 +0300

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+// var player = Vue.component("player", {
+// 	template: `<div>asdasd</div>`
+// });
+
+new Vue ({
+    el: "#app",
+    // components: {
+    //     player
+    // }
+    data () {
+        return {
+            testData: "asda"
+        }
+    }
+})
