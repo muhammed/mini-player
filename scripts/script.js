458f14e80fd84035063f103847abb241f53f3c6e
Muhammed Erdem <iamuhammederdem@gmail.com>
2019-08-25 12:17:22 +0300

    source data changed

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,6 @@ new Vue({
   el: "#app",
   data() {
     return {
-      testData: "asda",
       audio: null,
       circleLeft: null,
       barWidth: null,
@@ -15,62 +14,71 @@ new Vue({
           artist: "Norm Ender",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg",
           source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/1.mp3",
+          url: "https://www.youtube.com/watch?v=z3wAjJXbYzA",
           favorited: false
         },
         {
-          name: "The Upside 2",
-          artist: "Lindsey Stirling",
+          name: "Everybody Knows",
+          artist: "Leonard Cohen",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/2.jpg",
-          source: "https://customhtml5video.000webhostapp.com/audio.mp3",
+          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/2.mp3",
+          url: "https://www.youtube.com/watch?v=Lin-a2lTelg",
           favorited: true
         },
         {
-          name: "The Upside 3",
-          artist: "Lindsey Stirling",
+          name: "Extreme Ways",
+          artist: "Moby",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/3.jpg",
-          source: "https://customhtml5video.000webhostapp.com/audio.mp3",
+          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/3.mp3",
+          url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
           favorited: false
         },
         {
-          name: "The Upside 4",
-          artist: "Lindsey Stirling",
+          name: "Butterflies",
+          artist: "Sia",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/4.jpg",
-          source: "https://customhtml5video.000webhostapp.com/audio.mp3",
+          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/4.mp3",
+          url: "https://www.youtube.com/watch?v=kYgGwWYOd9Y",
           favorited: false
         },
         {
-          name: "The Upside 5",
-          artist: "Lindsey Stirling",
+          name: "The Final Victory",
+          artist: "Haggard",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/5.jpg",
-          source: "https://customhtml5video.000webhostapp.com/audio.mp3",
+          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/5.mp3",
+          url: "https://www.youtube.com/watch?v=0WlpALnQdN8",
           favorited: true
         },
         {
-          name: "The Upside 6",
-          artist: "Lindsey Stirling",
+          name: "Genius ft. Sia, Diplo, Labrinth",
+          artist: "LSD",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/6.jpg",
-          source: "https://customhtml5video.000webhostapp.com/audio.mp3",
+          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/6.mp3",
+          url: "https://www.youtube.com/watch?v=HhoATZ1Imtw",
           favorited: false
         },
         {
-          name: "The Upside 7",
-          artist: "Lindsey Stirling",
+          name: "The Comeback Kid",
+          artist: "Lindi Ortega",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/7.jpg",
-          source: "https://customhtml5video.000webhostapp.com/audio.mp3",
+          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/7.mp3",
+          url: "https://www.youtube.com/watch?v=me6aoX0wCV8",
           favorited: true
         },
         {
-          name: "The Upside 8",
-          artist: "Lindsey Stirling",
+          name: "Overdose",
+          artist: "Grandson",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/8.jpg",
-          source: "https://customhtml5video.000webhostapp.com/audio.mp3",
+          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/8.mp3",
+          url: "https://www.youtube.com/watch?v=00-Rl3Jlx-o",
           favorited: false
         },
         {
-          name: "The Upside 9",
-          artist: "Lindsey Stirling",
+          name: "Rag'n'Bone Man",
+          artist: "Human",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/9.jpg",
-          source: "https://customhtml5video.000webhostapp.com/audio.mp3",
+          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/9.mp3",
+          url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
           favorited: false
         }
       ],
